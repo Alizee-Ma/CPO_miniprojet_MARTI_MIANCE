698d3c2881ecd4f142fdcba5aeb2109dc8a979b3
Albane Miance <albane.miance@epfedu.fr>
2024-11-27 14:41:29 +0100

    Création de la classe Cellule Lumineuse et insertion des méthodes de la classe dans le main pour vérifier leurs bon fonctionnement

--- minijeu/src/minijeu/CelluleLumineuse.java
+++ minijeu/src/minijeu/CelluleLumineuse.java
@@ -9,5 +9,32 @@ package minijeu;
  * @author alize
  */
 public class CelluleLumineuse {
+    // Attribut pour représenter l'état de la cellule (allumée ou éteinte)
+    private boolean estAllumee; // Utilisation de 'private' pour assurer l'encapsulation
 
+    // Constructeur pour initialiser l'état de la cellule (par défaut éteinte)
+    public CelluleLumineuse() {
+        this.estAllumee = false; // La cellule est initialement éteinte
+    }
+
+    // Constructeur avec un paramètre pour initialiser l'état de la cellule
+    public CelluleLumineuse(boolean etatInitial) {
+        this.estAllumee = etatInitial; // Permet de définir un état initial (allumée ou éteinte)
+    }
+
+    // Méthode pour inverser l'état de la cellule
+    public void inverserEtat() {
+        estAllumee = !estAllumee; // Inverse l'état actuel : si allumée, devient éteinte, et vice-versa
+    }
+
+    // Méthode pour vérifier si la cellule est allumée
+    public boolean estAllumee() {
+        return estAllumee; // Retourne true si la cellule est allumée, sinon false
+    }
+
+    // Méthode pour représenter l'état de la cellule en tant que texte
+    @Override
+    public String toString() {
+        return estAllumee ? "Allumée" : "Éteinte"; // Renvoie une représentation textuelle de l'état
+    }
 }
--- minijeu/src/minijeu/Minijeu.java
+++ minijeu/src/minijeu/Minijeu.java
@@ -13,9 +13,23 @@ public class Minijeu {
     /**
      * @param args the command line arguments
      */
+         // Méthode main pour tester la classe CelluleLumineuse
     public static void main(String[] args) {
-        // TODO code application logic here
-    
+        // Création de cellules lumineuses et test des méthodes
+        CelluleLumineuse cellule1 = new CelluleLumineuse(); // Par défaut éteinte
+        CelluleLumineuse cellule2 = new CelluleLumineuse(true); // Initialisée à allumée
+
+        // Affichage de l'état initial des cellules
+        System.out.println("Cellule 1 - État initial : " + cellule1); // Devrait afficher "Éteinte"
+        System.out.println("Cellule 2 - État initial : " + cellule2); // Devrait afficher "Allumée"
+
+        // Inversion de l'état des cellules
+        cellule1.inverserEtat();
+        cellule2.inverserEtat();
+
+        // Affichage de l'état des cellules après inversion
+        System.out.println("Cellule 1 - Après inversion : " + cellule1); // Devrait afficher "Allumée"
+        System.out.println("Cellule 2 - Après inversion : " + cellule2); // Devrait afficher "Éteinte"
     }
     
 }
