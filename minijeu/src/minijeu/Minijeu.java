976a3377fd7c9fb45743f2cd5e550c031d5db458
Albane Miance <albane.miance@epfedu.fr>
2024-11-27 15:29:41 +0100

    Création de la classe Grille de cellule et insertion des méthodes de la classe dans le main pour vérifier leurs bon fonctionnement - Nous avons choisi des lignes et colonnes prédéfinies afin de tester le bon fonctionnement de la classe

--- minijeu/src/minijeu/Minijeu.java
+++ minijeu/src/minijeu/Minijeu.java
@@ -30,6 +30,26 @@ public class Minijeu {
         // Affichage de l'état des cellules après inversion
         System.out.println("Cellule 1 - Après inversion : " + cellule1); // Devrait afficher "Allumée"
         System.out.println("Cellule 2 - Après inversion : " + cellule2); // Devrait afficher "Éteinte"
+  
+   // Création d'une grille de cellules lumineuses et test d'affichage
+        GrilleDeCellule grille = new GrilleDeCellule(3); // Création d'une grille 3x3
+        System.out.println(grille); // Affichage de la grille
+
+        // Test des méthodes de GrilleDeCellules
+        grille.activerLigneDeCellules(0); // Active la ligne 0
+        System.out.println("Grille après activation de la ligne 0 :\n" + grille);
+
+        grille.activerColonneDeCellules(2); // Active la colonne 2
+        System.out.println("Grille après activation de la colonne 2 :\n" + grille);
+
+        grille.activerDiagonaleDescendante(); // Active la diagonale descendante
+        System.out.println("Grille après activation de la diagonale descendante :\n" + grille);
+
+        grille.activerDiagonaleMontante(); // Active la diagonale montante
+        System.out.println("Grille après activation de la diagonale montante :\n" + grille);
+
+        grille.melangerMatriceAleatoirement(5); // Mélange la grille avec 5 tours
+        System.out.println("Grille après mélange aléatoire :\n" + grille);
     }
-    
 }
+
